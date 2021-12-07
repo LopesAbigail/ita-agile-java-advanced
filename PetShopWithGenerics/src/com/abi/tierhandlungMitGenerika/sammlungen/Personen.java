9eba9d083ada847b76a63d7385b8c4db35d1db79
Abi <contatoabigail_lopes@outlook.com>
2021-12-07 17:19:35 -0300

    Add pearson class

--- PetShopWithGenerics/src/com/abi/tierhandlungMitGenerika/sammlungen/Personen.java
+++ PetShopWithGenerics/src/com/abi/tierhandlungMitGenerika/sammlungen/Personen.java
@@ -1,17 +1,17 @@
 package com.abi.tierhandlungMitGenerika.sammlungen;
 
-public class Tier {
+public class Personen {
     private final String name;
-    private final String farbe;
+    private final String jahreAlt;
 
-    public Tier(String name, String farbe){
+    public Personen(String name, String jahreAlt){
         super();
         this.name = name;
-        this.farbe = farbe;
+        this.jahreAlt = jahreAlt;
     }
 
     @Override
     public String toString() {
-        return "{ Hallo! Ich bin ein(e) " + farbe + " " + name + " }";
+        return "{ Hallo! Ich heisse " + name + " und Ich bin " + jahreAlt + " Jahre alt! }";
     }
 }
