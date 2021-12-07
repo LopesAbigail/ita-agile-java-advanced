d26da47f4cb018709a7a1dee951f98a1f7cc9f2d
Abi <contatoabigail_lopes@outlook.com>
2021-12-07 17:05:42 -0300

    Remove unnecessary files

--- PetShopWithGenerics/src/com/abi/tierhandlungMitGenerika/Tierhandlung.java
+++ PetShopWithGenerics/src/com/abi/tierhandlungMitGenerika/Tierhandlung.java
@@ -1,2 +1,23 @@
-package com.abi.tierhandlungMitGenerika;public class Tierhandlung {
+package com.abi.tierhandlungMitGenerika;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Tierhandlung<E> {
+
+    private List<E> list = new ArrayList<>();
+
+    public void add(E tier){
+        list.add(tier);
+    }
+
+    public E handhaben(){
+        Random r = new Random(System.currentTimeMillis());
+        return list.remove(r.nextInt(list.size()));
+    }
+
+    public boolean tiereHaben(){
+        return !list.isEmpty();
+    }
 }
--- PetShotWithGenerics/src/com/abi/Tier.java
+++
@@ -1,2 +0,0 @@
-package com.abi;public class Tier {
-}
