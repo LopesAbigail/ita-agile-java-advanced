94f981ea8d9a4eb01739349df5a7e0618db2d0ba
Abi <contatoabigail_lopes@outlook.com>
2022-01-20 11:50:23 -0300

    Add user DAO implementator

---
+++ DbConnection_Refact/src/com/abi/controller/UsuarioDAO.java
@@ -0,0 +1,116 @@
+package com.abi.controller;
+
+import com.abi.model.Usuario;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDAO implements IUsuarioDAO {
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void inserir(Usuario u) {
+        try (Connection c = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/coursera", "postgres", "postgres")){
+
+            String sql = "INSERT INTO usuario(login, email, nome, senha, pontos) VALUES (?, ?, ?, ?, ?);";
+
+            PreparedStatement stm = c.prepareStatement(sql);
+            stm.setString(1, u.getLogin());
+            stm.setString(2, u.getEmail());
+            stm.setString(3, u.getNome());
+            stm.setString(4, u.getSenha());
+            stm.setInt(5, u.getPontos());
+
+            stm.executeUpdate();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException("Ocorreu o seguinte erro ao tentar estabeler a conexao!", e);
+        }
+    }
+
+    @Override
+    public Usuario recuperar(String login) {
+        try (Connection c = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/coursera", "postgres", "postgres")){
+
+            String sql = "SELECT * FROM usuario WHERE login = ?;";
+
+            PreparedStatement stm = c.prepareStatement(sql);
+            stm.setString(1, login);
+
+            ResultSet rs = stm.executeQuery();
+
+            return new Usuario(
+                    rs.getString("login"),
+                    rs.getString("email"),
+                    rs.getString("nome"),
+                    rs.getString("senha"),
+                    rs.getInt("pontos"));
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException("Ocorreu o seguinte erro ao tentar estabeler a conexao!", e);
+        }
+    }
+
+    @Override
+    public void adicionarPontos(String login, int pontos) {
+        try (Connection c = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/coursera", "postgres", "postgres")){
+
+            String sql = "UPDATE usuario SET pontos = pontos + ? WHERE login = ?;";
+
+            PreparedStatement stm = c.prepareStatement(sql);
+            stm.setInt(1, pontos);
+            stm.setString(2, login);
+
+            stm.executeUpdate();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException("Ocorreu o seguinte erro ao tentar estabeler a conexao!", e);
+        }
+
+    }
+
+    @Override
+    public List<Usuario> ranking() {
+        List<Usuario> rank = new ArrayList<>();
+        try (Connection c = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/coursera", "postgres", "postgres")){
+
+            String sql = "SELECT * FROM usuario ORDER BY pontos DESC;";
+
+            PreparedStatement stm = c.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+
+            while (rs.next()){
+                Usuario u = new Usuario(
+                        rs.getString("login"),
+                        rs.getString("email"),
+                        rs.getString("nome"),
+                        rs.getString("senha"),
+                        rs.getInt("pontos"));
+
+                rank.add(u);
+            }
+
+            return rank;
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException("Ocorreu o seguinte erro ao tentar estabeler a conexao!", e);
+        }
+    }
+}
