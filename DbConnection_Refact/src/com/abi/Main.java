5cecc36f17f3969a59579fe6f34f4a5670d5ed68
Abi <contatoabigail_lopes@outlook.com>
2022-01-21 12:51:19 -0300

    Add test to user DAO implementator

--- DbConnection_Refact/src/com/abi/Main.java
+++ DbConnection_Refact/src/com/abi/Main.java
@@ -1,8 +1,24 @@
 package com.abi;
 
+import com.abi.controller.UsuarioDAO;
+import com.abi.model.Usuario;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Usuario anne = new Usuario(
+                "anne.gale", "anne.gale@mail.com", "Anne Pima Gale", "FTERWAJ87HG", 10);
+
+        Usuario robin = new Usuario(
+                "robin.gale", "robin.gale@mail.com", "Robin Pima Gale", "FTERWAJ87TY", 9);
+
+        UsuarioDAO dao = new UsuarioDAO();
+        dao.inserir(anne);
+        dao.inserir(robin);
+
+        List<Usuario> usuarios = dao.ranking();
+        usuarios.forEach(u -> System.out.println(u.toString()));
     }
 }
---
+++ DbConnection_Refact/test/TestUsuarioDAO.java
@@ -0,0 +1,12 @@
+package com.abi.controller;
+
+import com.abi.model.Usuario;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUsuarioDAO {
+
+
+}
