68ef24194bbce972ae240f3ef08a6529e004c38f
Abi <contatoabigail_lopes@outlook.com>
2022-01-28 13:16:56 -0300

    Add servlet in the view package

---
+++ GameProject/src/java/view/LoginServlet.java
@@ -0,0 +1,44 @@
+package view;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import controller.UsuarioDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author conta
+ */
+@WebServlet(urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String LOGIN = request.getParameter("login");
+        String SENHA = request.getParameter("senha");
+        
+        UsuarioDAO auth = new UsuarioDAO();
+        try {
+            String nomeUsuario = auth.getNomeUsuario(LOGIN, SENHA);
+            request.setAttribute("nome", nomeUsuario);
+            request.getRequestDispatcher("sucesso.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("erro", ex.getMessage());
+            request.getRequestDispatcher("erro.jsp").forward(request, response);
+        }
+    }
+
+}
