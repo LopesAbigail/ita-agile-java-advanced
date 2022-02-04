f8b6bc556c54d4594b4a18ca5ee0a27aeac2ac8f
Abi <contatoabigail_lopes@outlook.com>
2022-02-04 16:53:25 -0300

    Add fields references

--- GameProject/src/java/CreateLoginServlet.java
+++ GameProject/src/java/CreateLoginServlet.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import controller.UsuarioDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -10,18 +11,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Usuario;
 
 /**
  *
  * @author conta
  */
-@WebServlet(urlPatterns = {"/createLogin"})
+@WebServlet(urlPatterns = {"/create"})
 public class CreateLoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        UsuarioDAO auth = new UsuarioDAO();
         
+        String nome = request.getParameter("nome");
+        String usuario = request.getParameter("usuario");
+        String senha = request.getParameter("senha");
+        
+        String email = usuario + "@mail.com";
+        
+        try {
+            auth.inserir(new Usuario(usuario, email, nome, senha, 0));
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e);
+        }
     }
 
 }
