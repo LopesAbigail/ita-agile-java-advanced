adf2c56231c76af18bd162a286db9db9382935b4
Abi <contatoabigail_lopes@outlook.com>
2022-02-08 17:46:05 -0300

    Add Comentario model

---
+++ GameProject/src/java/model/Comentario.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author conta
+ */
+public class Comentario {
+    
+    private String comentario;
+    private String login;
+    private int topicoId;
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTopicoId() {
+        return topicoId;
+    }
+
+    public void setTopicoId(int topicoId) {
+        this.topicoId = topicoId;
+    }
+    
+    
+    
+}
